f5da756311acdd497d4a02d44574086400b28bb0
Bee <github@skerritt.blog>
2021-12-23 00:13:04 +0000

    ci=false

--- business-card/src/Data/Github.js
+++ business-card/src/Data/Github.js
@@ -1,14 +1,11 @@
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
-function GitHub() {
-    return ([
-        {
+export default [
+    {
         id: 1,
         title: "Ciphey",
         githubStars: 8900,
         description: "Automated decryption tool using AI & NLP",
         coverImg: "ciphey.png",
         link: "https://github.com/ciphey/ciphey"
-    }])
-}
-
-export default GitHub
+    },
+]
