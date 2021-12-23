253fbae9d806f2890360cd9a296ceb8e31bda91e
Bee <github@skerritt.blog>
2021-12-23 10:06:31 +0000

    add link to header image

--- business-card/src/components/Header.js
+++ business-card/src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
     return (
         <div className="App-header">
             <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0"/>
-            <img src={logo} className="App-logo" alt="logo" />
+            <a href="https://0xbee.eth.xyz"><img src={logo} className="App-logo" alt="logo" /> </a>
             <h1 className="Header-Name"><a href="https://0xbee.eth.xyz">0xbee.eth</a></h1>
             <h2 className="Header-tagline">Building the future of finance @<a href="https://monzo.com">Monzo</a></h2>
             <h3 className=""><a href = "https://github.com/bee-san">github.com/bee-san</a></h3>
