6e19ae9a98027833053bebea7c61a2a537bf3602
Bee <github@skerritt.blog>
2022-07-27 09:37:01 +0100

    Update github star numbers

--- business-card/src/Data/Github.js
+++ business-card/src/Data/Github.js
@@ -1,46 +1,50 @@
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 const GitHub = [
-    {
-        id: 1,
-        title: "Ciphey",
-        githubStars: "⭐️ 8900 Stars",
-        description: "⚡ Automatically decrypt encryptions without knowing the key or cipher, decode encodings, and crack hashes ⚡ Uses AI & NLP.",
-        coverImg: "ciphey.png",
-        link: "https://github.com/ciphey/ciphey"
-    },
-    {
-        id: 2,
-        title: "RustScan",
-        githubStars: "⭐️ 5372 Stars",
-        description: "The future of Port Scanning, the world's fastest portscanner without spending $2000 on networking equipment.",
-        coverImg: "rustscan.svg",
-        link: "https://github.com/rustscan/rustscan"
-    },
-    {
-        id: 3,
-        title: "PyWhat",
-        githubStars: "⭐️ 5004 Stars",
-        description: "🐸 Identify anything. pyWhat easily lets you identify emails, IP addresses, and more. Feed it a .pcap file or some text and it'll tell you what it is! 🧙‍♀️",
-        coverImg: "pywhat.svg",
-        link: "https://github.com/bee-san/pywhat"
-    },
-    {
-        id: 4,
-        title: "Name-That-Hash",
-        githubStars: "⭐️ 932 Stars",
-        description: "🔗 Don't know what type of hash it is? Name That Hash will name that hash type! 🤖 Identify MD5, SHA256 and 300+ other hashes ☄ ",
-        coverImg: "nth.svg",
-        link: "https://github.com/HashPals/Name-That-Hash"
-    },
-    {
-        id: 5,
-        title: "Search-That-Hash",
-        githubStars: "⭐️ 898 Stars",
-        description: "🔎Searches Hash APIs to crack your hash quickly🔎 If hash is not found, automatically pipes into HashCat⚡",
-        coverImg: "searchthathash.svg",
-        link: "https://github.com/HashPals/Search-That-Hash"
-    },
+  {
+    id: 1,
+    title: "Ciphey",
+    githubStars: "⭐️ 10.4k Stars",
+    description:
+      "⚡ Automatically decrypt encryptions without knowing the key or cipher, decode encodings, and crack hashes ⚡ Uses AI & NLP.",
+    coverImg: "ciphey.png",
+    link: "https://github.com/ciphey/ciphey",
+  },
+  {
+    id: 2,
+    title: "RustScan",
+    githubStars: "⭐️ 6.7k Stars",
+    description:
+      "The future of Port Scanning, the world's fastest portscanner without spending $2000 on networking equipment.",
+    coverImg: "rustscan.svg",
+    link: "https://github.com/rustscan/rustscan",
+  },
+  {
+    id: 3,
+    title: "PyWhat",
+    githubStars: "⭐️ 5.3k Stars",
+    description:
+      "🐸 Identify anything. pyWhat easily lets you identify emails, IP addresses, and more. Feed it a .pcap file or some text and it'll tell you what it is! 🧙‍♀️",
+    coverImg: "pywhat.svg",
+    link: "https://github.com/bee-san/pywhat",
+  },
+  {
+    id: 4,
+    title: "Name-That-Hash",
+    githubStars: "⭐️ 1.1k Stars",
+    description:
+      "🔗 Don't know what type of hash it is? Name That Hash will name that hash type! 🤖 Identify MD5, SHA256 and 300+ other hashes ☄ ",
+    coverImg: "nth.svg",
+    link: "https://github.com/HashPals/Name-That-Hash",
+  },
+  {
+    id: 5,
+    title: "Search-That-Hash",
+    githubStars: "⭐️ 1k Stars",
+    description:
+      "🔎Searches Hash APIs to crack your hash quickly🔎 If hash is not found, automatically pipes into HashCat⚡",
+    coverImg: "searchthathash.svg",
+    link: "https://github.com/HashPals/Search-That-Hash",
+  },
+];
 
-]
-
-export default GitHub
+export default GitHub;
